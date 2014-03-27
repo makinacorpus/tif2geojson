9b768d65bdd00c8a3bc2bb3675add58735b3e37f
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2014-03-27 13:02:57 +0100

    Language should work lower case too

--- tif2geojson.py
+++ tif2geojson.py
@@ -93,7 +93,7 @@ class Converter(object):
         descriptions = _deep_value_list(entry, 'tif:DublinCore', 'dc:description',
                                         default=[])
         for description in descriptions:
-            if description.get('@xml:lang') == self.lang:
+            if description.get('@xml:lang').lower() == self.lang.lower():
                 return description.get('#text')
 
     def _parse_communication_media(self, entry, contact_type=None):
